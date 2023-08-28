641cc0ecaac0f04c7422326e020f517652ba7af6
mrrobot911 <kelwin2705@gmail.com>
2023-08-28 10:56:19 +0300

    feat:init

---
+++ src/guessing-game.js
@@ -0,0 +1,27 @@
+class GuessingGame {
+    constructor() {
+        this.min = 0
+        this.max = 0
+        this.number = 0
+    }
+
+    setRange(min, max) {
+        this.min = min
+        this.max = max
+    }
+
+    guess() {
+        this.number = Math.ceil((this.max + this.min)/2);
+        return this.number
+    }
+
+    lower() {
+        this.max = this.number
+    }
+
+    greater() {
+        this.min = this.number
+    }
+}
+
+module.exports = GuessingGame;
